c8b7dcbd46b034c3b1182e063bf0b84c11fd113a
Tim Luu <timbo.luu@gmail.com>
2018-12-21 21:59:02 -0800

    Complete multiply

--- src/recursion.js
+++ src/recursion.js
@@ -149,6 +149,15 @@ var modulo = function(x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+	if(y === 0 || x === 0) {
+	  return 0;
+	} else if(y === 1) {
+	  return x;
+	} else if(y < 0) {
+	  return -(multiply(x, -y));
+	} else {
+	  return x + multiply(x, y-1);
+	}
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
